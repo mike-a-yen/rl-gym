2424218007eb8b1b249a4983685d8c2d5d14ebbb
mike-a-yen <mike.a.yen@gmail.com>
2021-04-03 21:09:25 -0700

    run on cuda:1

--- callbacks/__init__.py
+++ callbacks/__init__.py
@@ -1,7 +1,6 @@
 from .callback import Callback
 from .callback_runner import CallbackRunner
 from .model_checkpoint import ModelCheckpoint
-from .model_visualizer import ConvWeightVisualizer
 from .replay_buffer import ReplayBuffer
 from .target_model_updater import TargetModelUpdater
 from .wandb_logger import WandBLogger
